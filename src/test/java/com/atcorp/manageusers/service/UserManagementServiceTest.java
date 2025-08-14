784d35dda769ad3f650c2b50500e4eb98f9e5e78
Ajinkya Tanksale <tanksale.ajinkya@gmail.com>
2025-08-14 17:14:40 +0530

    Added test cases for service layer login method

--- src/test/java/com/atcorp/manageusers/service/UserManagementServiceTest.java
+++ src/test/java/com/atcorp/manageusers/service/UserManagementServiceTest.java
@@ -1,21 +1,28 @@
 package com.atcorp.manageusers.service;
 
+import com.atcorp.manageusers.configurations.JwtHelper;
 import com.atcorp.manageusers.dao.UserManagementDao;
 import com.atcorp.manageusers.entity.UserEntity;
 import com.atcorp.manageusers.model.User;
 import com.atcorp.manageusers.service.UserManagementServiceImpl;
+import com.atcorp.manageusers.utils.exception.AuthenticationFailureException;
 import com.atcorp.manageusers.utils.exception.UserAlreadyExistsException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -33,6 +40,12 @@ public class UserManagementServiceTest {
 
     @InjectMocks
     UserManagementServiceImpl userManagementServiceImpl;
+
+    @Mock
+    JwtHelper jwtHelper;
+
+    @Mock
+    AuthenticationManager authenticationManager;
 
     @Test
     public void testEnrollmentSuccessful () throws ParseException {
@@ -89,6 +102,67 @@ public class UserManagementServiceTest {
 
         assertThrows(ParseException.class, () -> {
             userManagementServiceImpl.createCustomer(user);
+        });
+    }
+
+    @Test
+    public void testLoginSuccessful () throws ParseException {
+        User user = new User("tanksale.ajnkya@gmail.com", "Ajinkya@1012", "10/12/1998", "Male", "9762289985");
+        String formatPattern = "dd/MM/yyyy HH:mm:ss";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(formatPattern);
+        Date parsedDate = dateFormat.parse(user.getDob() + " 00:00:00");
+        Timestamp timestamp = new Timestamp(parsedDate.getTime());
+        UserEntity existingUser = new UserEntity.UserEntityBuilder().setUserName("tanksale.ajnkya@gmail.com")
+                .setPassword("Ajinkya@1012")
+                .setDob(timestamp)
+                .setGender("Male")
+                .setPhoneNumber("9762289985").build();
+
+        UsernamePasswordAuthenticationToken authToken =
+                new UsernamePasswordAuthenticationToken(
+                        "tanksale.ajinkya@gmail.com",        // principal
+                        "Ajinkya@1012",        // credentials
+                        List.of(new SimpleGrantedAuthority("USER")) // authorities
+                );
+
+        when(jwtHelper.generateToken(user)).thenReturn("jwtToken");
+        when(userManagementDao.findByUserName(any(String.class))).thenReturn(existingUser);
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(authToken);
+
+        String result = userManagementServiceImpl.loginCustomer(user);
+
+        assertEquals("jwtToken", result);
+    }
+
+    @Test
+    public void testLoginFailureUsernameNotFound () {
+        User user = new User("tanksale.ajnkya@gmail.com", "Ajinkya@1012", "10/12/1998", "Male", "9762289985");
+
+        when(userManagementDao.findByUserName(any(String.class))).thenReturn(null);
+
+        assertThrows(UsernameNotFoundException.class, () -> {
+            userManagementServiceImpl.loginCustomer(user);
+        });
+    }
+
+    @Test
+    public void testLoginFailureAuthenticationFailure () throws ParseException {
+        User user = new User("tanksale.ajnkya@gmail.com", "Ajinkya@1012", "10/12/1998", "Male", "9762289985");
+        String formatPattern = "dd/MM/yyyy HH:mm:ss";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(formatPattern);
+        Date parsedDate = dateFormat.parse(user.getDob() + " 00:00:00");
+        Timestamp timestamp = new Timestamp(parsedDate.getTime());
+        UserEntity existingUser = new UserEntity.UserEntityBuilder().setUserName("tanksale.ajnkya@gmail.com")
+                .setPassword("Ajinkya@1012")
+                .setDob(timestamp)
+                .setGender("Male")
+                .setPhoneNumber("9762289985").build();
+
+        when(userManagementDao.findByUserName(any(String.class))).thenReturn(existingUser);
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(new UsernamePasswordAuthenticationToken(user, null));
+
+        assertThrows(AuthenticationFailureException.class, () -> {
+            userManagementServiceImpl.loginCustomer(user);
         });
     }
 
