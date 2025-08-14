9eb2cddcb8a52054f777239b5076e0618a0590de
Ajinkya Tanksale <tanksale.ajinkya@gmail.com>
2025-08-14 20:51:11 +0530

    Dockerized the application

--- src/main/java/com/atcorp/manageusers/ManageusersApplication.java
+++ src/main/java/com/atcorp/manageusers/ManageusersApplication.java
@@ -2,11 +2,18 @@ package com.atcorp.manageusers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
-public class ManageusersApplication {
+public class ManageusersApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ManageusersApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManageusersApplication.class, args);
